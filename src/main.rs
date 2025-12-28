286728df618b02ca3322904aafc09ce9108336b9
Cristian Zoz <cristian@zoz.dev>
2025-12-28 18:21:38 +0100

    chore: base clap structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+mod commands;
+
+#[derive(Parser)]
+#[command(name = "cudup", author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install { version: Option<String> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Install { version } => commands::install(version),
+    }
 }
