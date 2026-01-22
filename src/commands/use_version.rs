1d5a75c44fef35e5eb127cffc5bbd73d77d761db
Cristian Zoz <cristian@zoz.dev>
2026-01-23 00:20:07 +0100

    refactor: replace unnecessary 'log'

--- src/commands/use_version.rs
+++ src/commands/use_version.rs
@@ -1,11 +1,8 @@
 use anyhow::{Result, bail};
-use log::info;
 
 use crate::fetch;
 
-/// Generates shell commands to activate a specific CUDA version
 pub fn use_version(version: &str) -> Result<()> {
-    // Check if the version is installed locally
     let install_dir = fetch::version_install_dir(version)?;
     if !install_dir.exists() {
         bail!(
@@ -16,13 +13,10 @@ pub fn use_version(version: &str) -> Result<()> {
         );
     }
 
-    // Generate shell export commands (eval'd by shell function from `cudup setup`)
-    let cuda_home = install_dir.display();
-    println!("export CUDA_HOME=\"{}\"", cuda_home);
+    println!("# CUDA {} activated", version);
+    println!("export CUDA_HOME=\"{}\"", install_dir.display());
     println!("export PATH=\"$CUDA_HOME/bin${{PATH:+:$PATH}}\"");
     println!("export LD_LIBRARY_PATH=\"$CUDA_HOME/lib64${{LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}}\"");
-
-    info!("CUDA {} activated", version);
 
     Ok(())
 }
